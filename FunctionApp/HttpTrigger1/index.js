5b6fce00198b56bb0045bc1ba78bd598492acf27
Will Eastbury <wieastbu@microsoft.com>
2021-09-28 14:32:04 +0100

    completed addition

---
+++ FunctionApp/HttpTrigger1/index.js
@@ -0,0 +1,23 @@
+module.exports = async function (context, req) {
+    
+    const csdl = require("odata-csdl");
+    const lib = require("../odata-openapi/lib/csdl2openapi");
+    const xml = req.body;
+    const json = csdl.xml2json(xml, false);
+
+    const openapi = lib.csdl2openapi(json, {
+      scheme: req.headers['x-scheme'] || "https",
+      host: req.headers['x-host'] || "services.odata.org",
+      basePath: req.headers['x-basePath'] || "/service-root",
+      diagram: true,
+    });
+
+    const responseMessage = JSON.stringify(openapi, null, 4);
+
+    context.res = {
+        status: 200,
+        headers: {'content-type': 'application/json'},
+        body: responseMessage
+    };
+
+}
