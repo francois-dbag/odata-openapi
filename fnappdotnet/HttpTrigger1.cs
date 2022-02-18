ba6a7f6c8725762e5860962748754db5c3e2e866
Will Eastbury <wieastbu@microsoft.com>
2022-02-18 16:37:48 +0000

    UPLOADING monkey-patches for Powerplat and v3

--- fnappdotnet/HttpTrigger1.cs
+++ fnappdotnet/HttpTrigger1.cs
@@ -78,99 +78,187 @@ namespace Company.Function
                     // (OpenAPI's types are based on JSON Schema which has no concept of a 'decimal' data type)
                     // and Power Platform has an issue with the converter's output IF there is an EDM:Decimal in the OData spec.
                     JToken jtroot = null; 
-                    
+                    var PatchList = new List<PatchData>(); 
+
                     if ((req.Headers["x-openapi-version"].FirstOrDefault() ?? "3.0.0").StartsWith("2.0"))
                     {
                         // OpenAPI2.0 (Swagger)
-                         jtroot = openapi["definitions"];                     // definitions['GWSAMPLE_BASIC.Product'].properties.WeightMeasure.anyOf[0]"
-                    }
-                       
-                    if ((req.Headers["x-openapi-version"].FirstOrDefault() ?? "3.0.0").StartsWith("3.0"))
-                    {
-                        // OpenAPI3.0
-                        jtroot = openapi["definitions"]["schemas"]; 
-                    }
-
-                    var PatchList = new List<PatchData>(); 
-                           
-                    foreach (JProperty jdefinition in jtroot)
-                    {
-                        // At this point we are looping through each object definition (So in each of definitions['x'])
-                        foreach (JProperty jpropertyobject in jdefinition.Value)
+                        // "WeightMeasure": {   // Looking for a string in the 'type' property and a format 'decimal' value
+                        //          "type": [
+                        //             "number",
+                        //             "string", 
+                        //             "null"
+                        //              ],
+                        //          "format": "decimal"
+                        //         },
+                         jtroot = openapi["definitions"];                     
+                                                   
+                        foreach (JProperty jdefinition in jtroot)
                         {
-                            if(jpropertyobject.Name == "properties") // Now we are looking for a child property object (So definitions['x'].properties)
+                            // At this point we are looping through each object definition (So in each of definitions['x'])
+                            foreach (JProperty jpropertyobject in jdefinition.Value)
                             {
-                                foreach (JProperty propertyfieldinner2 in jpropertyobject.Value)
+                                if(jpropertyobject.Name == "properties") // Now we are looking for a child property object (So definitions['x'].properties)
                                 {
-                                    foreach (var propertyfieldinner3 in propertyfieldinner2) // Now we are looking through the fields of that object 
+                                    foreach (JProperty propertyfieldinner2 in jpropertyobject.Value)
                                     {
-                                        // "WeightMeasure": {   // Looking for a string in the 'type' property and a format 'decimal' value
-                                        //          "type": [
-                                        //             "number",
-                                        //             "string", 
-                                        //             "null"],
-                                        //          "format": "decimal"
-                                        //         },
-
-                                        // Now parse the type and format of that object
-                                        bool isString = propertyfieldinner3["type"]?.ToString() == "string"; 
-                                        bool hasDecimal = propertyfieldinner3["format"]?.ToString() == "decimal"; 
-                                        bool isArrayAndHasString = false;
-                                        bool isNullable = false;
-                                        
-                                        // If the type property is an array then parse the array values to check for a string value
-                                        // IsString will be true if there is only one property called 'string'.
-                                        // But here we look for that value as an element in the array
-                                        if(!isString && (propertyfieldinner3["type"]?.Count() > 1))
+                                        foreach (var propertyfieldinner3 in propertyfieldinner2) // Now we are looking through the fields of that object 
                                         {
-                                            foreach (var prop in propertyfieldinner3["type"])
+
+                                            // Now parse the type and format of that object
+                                            bool isString = propertyfieldinner3["type"]?.ToString() == "string"; 
+                                            bool hasDecimal = propertyfieldinner3["format"]?.ToString() == "decimal"; 
+                                            bool isArrayAndHasString = false;
+                                            bool isNullable = false;
+                                            
+                                            // If the type property is an array then parse the array values to check for a string value
+                                            // IsString will be true if there is only one property called 'string'.
+                                            // But here we look for that value as an element in the array
+                                            if(!isString && (propertyfieldinner3["type"]?.Count() > 1))
                                             {
-                                                if(prop.ToString() == "null")
+                                                foreach (var prop in propertyfieldinner3["type"])
                                                 {
-                                                    isNullable = true;
-                                                }
-                                                if(prop.ToString() == "string") 
-                                                {
-                                                    isArrayAndHasString = true;
+                                                    if(prop.ToString() == "null")
+                                                    {
+                                                        isNullable = true;
+                                                    }
+                                                    if(prop.ToString() == "string") 
+                                                    {
+                                                        isArrayAndHasString = true;
+                                                    }
                                                 }
                                             }
-                                        }
 
-                                        // Detection complete, time to monkey-patch the json and replace the string type with a float type
-                                        if (hasDecimal)
-                                        {
-                                            PatchList.Add(new PatchData() {
-                                                Path = propertyfieldinner3["type"].Path, 
-                                                IsString = isString,
-                                                IsArrayAndHasString = isArrayAndHasString,
-                                                IsNullable = isNullable
-                                            });
+                                            // Detection complete, time to monkey-patch the json and replace the string type with a float type
+                                            if (hasDecimal)
+                                            {
+                                                PatchList.Add(new PatchData() {
+                                                    Path = propertyfieldinner3["type"].Path, 
+                                                    IsString = isString,
+                                                    IsArrayAndHasString = isArrayAndHasString,
+                                                    IsNullable = isNullable
+                                                });
 
-                                            log.LogInformation($"{propertyfieldinner3["type"].Path}, IsString: {isString}, IsArrayAndHasString: {isArrayAndHasString}, IsNullable: isNullable");
+                                                log.LogInformation($"{propertyfieldinner3["type"].Path}, IsString: {isString}, IsArrayAndHasString: {isArrayAndHasString}, IsNullable: isNullable");
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
-                    }
-
-                    // We need to do the actual patching here. 
-                    foreach (PatchData patchme in PatchList)
-                    {
-                        if (patchme.IsString)
+                    
+                        // We need to do the actual patching here. 
+                        foreach (PatchData patchme in PatchList)
                         {
-                            openapi.SelectToken(patchme.Path, true).Replace(new JValue(patchToWhat));
+                            if (patchme.IsString)
+                            {
+                                openapi.SelectToken(patchme.Path, true).Replace(new JValue(patchToWhat));
+                            }
+
+                            else if (patchme.IsArrayAndHasString)
+                            {
+                                JArray tmparray = new JArray(); 
+                                foreach (JValue jv in (JArray) openapi.SelectToken(patchme.Path, true))
+                                {
+                                    if (jv.ToString() != "string" && jv.ToString() != "number") tmparray.Add(jv);
+                                }
+                                tmparray.Add(new JValue(patchToWhat));
+                                openapi.SelectToken(patchme.Path, true).Replace(tmparray);
+                            }
                         }
 
-                        else if (patchme.IsArrayAndHasString)
+                    }
+                       
+                    if ((req.Headers["x-openapi-version"].FirstOrDefault() ?? "3.0.0").StartsWith("3.0"))
+                    {
+                        // OpenAPI3.0
+                        // WeightMeasure": {
+                        // "anyOf": [
+                        //     {
+                        //         "type": "number"
+                        //     },
+                        //     {
+                        //         "type": "string"
+                        //     }
+                        // ],
+                        // "nullable": true,
+                        // "format": "decimal",
+
+                        jtroot = openapi["components"]["schemas"]; 
+
+                        foreach (JProperty jdefinition in jtroot)
                         {
-                            JArray tmparray = new JArray(); 
-                            foreach (JValue jv in (JArray) openapi.SelectToken(patchme.Path, true))
+                            // At this point we are looping through each object definition (So in each of definitions['x'])
+                            foreach (JProperty jpropertyobject in jdefinition.Value)
                             {
-                                if (jv.ToString() != "string" && jv.ToString() != "number") tmparray.Add(jv);
+                                if(jpropertyobject.Name == "properties") // Now we are looking for a child property object (So definitions['x'].properties)
+                                {
+                                    foreach (JProperty propertyfieldinner2 in jpropertyobject.Value)
+                                    {
+                                        foreach (var propertyfieldinner3 in propertyfieldinner2) // Now we are looking through the fields of that object 
+                                        {
+                                            // Is it a reference to another object ? 
+                                            if(propertyfieldinner3.Children().Count() > 1)
+                                            {
+                                                // Now parse the type and format of that object
+                                                bool hasDecimal = propertyfieldinner3["format"]?.ToString() == "decimal"; 
+                                                bool isArrayAndHasString = false;
+
+                                                JArray typesanyof = (JArray) propertyfieldinner3["anyOf"] ?? new JArray(); 
+
+                                                foreach (JObject typedescr in typesanyof)
+                                                {
+                                                    if (typedescr.GetValue("type").ToString() == "string") 
+                                                    {
+                                                             isArrayAndHasString = true;
+                                                    }
+                                                }
+
+                                                // Detection complete, time to monkey-patch the json and replace the string type with a float type
+                                                if (hasDecimal)
+                                                {
+                                                    PatchList.Add(new PatchData() {
+                                                        Path = propertyfieldinner3["anyOf"].Path, 
+                                                        IsArrayAndHasString = isArrayAndHasString,
+                                                    });
+
+                                                    log.LogInformation($"{propertyfieldinner3["anyOf"].Path}, IsArrayAndHasString: {isArrayAndHasString}");
+                                                }
+                                                
+                                            }
+
+                                        }
+                                    }
+                                }
                             }
-                            tmparray.Add(new JValue(patchToWhat));
-                            openapi.SelectToken(patchme.Path, true).Replace(tmparray);
+                        }
+
+                        // We need to do the actual patching here. 
+                        foreach (PatchData patchme in PatchList)
+                        {
+                            if (patchme.IsArrayAndHasString)
+                            {
+                                JArray tmparray = new JArray(); 
+                                foreach (JObject jv in (JArray) openapi.SelectToken(patchme.Path, true))
+                                {                              
+
+                                    if (jv.GetValue("type").ToString() != "string" && jv.GetValue("type").ToString() != "number") tmparray.Add(jv);
+
+                                }
+                                if (tmparray.Count() != 0) // if this is zero the structure changes from an anyOf array to simply a 'type' property.
+                                {
+                                    JObject patch = new JObject();
+                                    patch.Add("type", new JValue(patchToWhat));
+                                    tmparray.Add(patch);
+                                    openapi.SelectToken(patchme.Path, true).Replace(tmparray);
+                                }
+                                else
+                                {
+                                    var old = openapi.SelectToken(patchme.Path, true).Parent.Parent;
+                                    old.AddFirst(new JProperty("type", new JValue(patchToWhat))); 
+                                    openapi.SelectToken(patchme.Path, true).Parent.Remove();
+                                }
+                            }
                         }
                     }
                 }
