b717597db47a153ff2b1dd9ee11d3fbee2647119
Will Eastbury <wieastbu@microsoft.com>
2022-03-01 20:38:10 +0000

    Code Cleanup and refactor + comments.

---
+++ fnappdotnet/OpenAPIConverterAndPatches.cs
@@ -0,0 +1,140 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Xml.Xsl;
+using System.Xml;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.OpenApi.Readers;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Any;
+namespace OpenApi.Converter {
+    public class OpenAPIConverterAndPatches {
+        private XslCompiledTransform v2toV4xsl = null, v4CSDLToOpenAPIXslt = null, CSDLToODataVersion = null; 
+        [FunctionName("HttpTrigger1")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log) {
+            string root = ""; 
+            string inputData = await req.ReadAsStringAsync(); 
+            if (bool.Parse(System.Environment.GetEnvironmentVariable("OverrideLocalTransformFilesInRoot") ?? "false")){
+                root = Path.Combine(System.Environment.GetEnvironmentVariable("HOME")); // Debugging override option
+            }
+            else{   
+                root = Path.Combine(System.Environment.GetEnvironmentVariable("HOME"), "site", "wwwroot"); // is running in app service / functions}
+            }
+            if (v2toV4xsl == null) { // First run on this host - caching stylesheets and transforms 
+                XslCompiledTransform v2toV4xsl = new XslCompiledTransform(); v2toV4xsl.Load(Path.Combine(root, "V2-to-V4-CSDL.xsl"));
+                XslCompiledTransform v4CSDLToOpenAPIXslt = new XslCompiledTransform(); v4CSDLToOpenAPIXslt.Load(Path.Combine(root, "V4-CSDL-to-OpenAPI.xsl")); 
+                XslCompiledTransform CSDLToODataVersion = new XslCompiledTransform(); CSDLToODataVersion.Load(Path.Combine(root, "OData-Version.xsl")); 
+            }
+            string v4OdataXml = ApplyTransform(inputData, v2toV4xsl);
+            string inputVersion = ApplyTransform(inputData, CSDLToODataVersion);
+            XsltArgumentList args = new XsltArgumentList();
+            args.AddParam("scheme","", (req.Headers["x-scheme"].FirstOrDefault() ?? "") == "" ? "https" : req.Headers["x-scheme"].FirstOrDefault());
+            args.AddParam("host","", (req.Headers["x-host"].FirstOrDefault() ?? "") == "" ? "services.odata.org" : req.Headers["x-host"].FirstOrDefault());
+            args.AddParam("basePath","", (req.Headers["x-basepath"].FirstOrDefault() ?? "") == "" ? "/service-root" : req.Headers["x-basepath"].FirstOrDefault());
+            args.AddParam("odata-version","", inputVersion); 
+            args.AddParam("diagram","","YES");
+            args.AddParam("openapi-root","", "https://raw.githubusercontent.com/oasis-tcs/odata-openapi/master/examples/");     
+            args.AddParam("openapi-version","" , "3.0.0"); 
+            OpenApiDocument openApiDocument = new OpenApiStringReader().Read(ApplyTransform(v4OdataXml, v4CSDLToOpenAPIXslt, args), out var diagnostic);
+            if (bool.Parse(req.Headers["x-openapi-enrich-decimals"].FirstOrDefault() ?? "false")){ // PowerPlatform doesn't like the multipleOf property on decimal types
+                foreach(OpenApiSchema schemaentry in openApiDocument.Components.Schemas.Values){
+                    foreach(KeyValuePair<string, OpenApiSchema> schemasubentry in schemaentry.Properties){
+                        OpenApiSchema thisSchema = schemasubentry.Value; // If just a string type with a decimal signature, null out the multipleOf property
+                        if  ((thisSchema.Type == "string" && thisSchema.Format == "decimal") || ((thisSchema.AnyOf.Where(e => e.Type == "string").Count() > 1) && thisSchema.Format == "decimal")){
+                            thisSchema.MultipleOf = null;
+                        }
+                    }
+                }
+            }
+            if (bool.Parse(req.Headers["x-openapi-ifmatch"].FirstOrDefault() ?? "false")){ // Add the if-match header for concurrency control if needed
+                foreach(KeyValuePair<string, OpenApiPathItem> pathitem in openApiDocument.Paths){
+                    foreach(KeyValuePair<OperationType,OpenApiOperation> operation in pathitem.Value.Operations){
+                        var ifMatchParam = new OpenApiParameter(){
+                            Name = "if-match",
+                            In = ParameterLocation.Header, 
+                            Required = true,
+                            Schema = new OpenApiSchema() {
+                                Type = "string"
+                            }
+                        }; 
+                        ifMatchParam.AddExtension("x-ms-visibility", new OpenApiString("required")); // Make this field appear in powerplatform's gui for the connector
+                        ifMatchParam.AddExtension("x-ms-summary", new OpenApiString("Place the eTag value for optimistic concurrency control in this header"));
+                        switch (operation.Key){
+                            case OperationType.Patch : 
+                                operation.Value.Parameters.Add(ifMatchParam);
+                                break;
+                            case OperationType.Delete : 
+                                operation.Value.Parameters.Add(ifMatchParam);
+                                break;
+                        } 
+                    }
+                }
+            }
+            if (bool.Parse(req.Headers["x-openapi-add-metadata-etags"].FirstOrDefault() ?? "false")){ // Add the etag entries to the schema so optimistic concurrency can work
+                foreach (KeyValuePair<string, OpenApiSchema> schemaDictionaryEntry in openApiDocument.Components.Schemas){
+                    OpenApiSchema meta = new OpenApiSchema(){ Type = "object"};
+                    meta.Properties.Add("etag", new OpenApiSchema(){
+                        Title = "The optimistic concurrency etag field, specify this in an if-match header", 
+                        Nullable = true,
+                        Type = "string"
+                    });
+                    schemaDictionaryEntry.Value.Properties.Add("__metadata", meta);
+                }
+            }
+            if (bool.Parse(req.Headers["x-openapi-enrich-tags"].FirstOrDefault() ?? "false")){ // Copy the description over to the tags
+                foreach (OpenApiTag tag in openApiDocument.Tags) tag.Description = tag.Name; 
+            }
+            if (bool.Parse(req.Headers["x-openapi-pivot-for-autorest"].FirstOrDefault() ?? "false")){
+                foreach(KeyValuePair<string, OpenApiPathItem> pathitem in openApiDocument.Paths){// Add unique operationId for each operation based on the 'summary' field
+                    foreach(KeyValuePair<OperationType,OpenApiOperation> operation in pathitem.Value.Operations){
+                        operation.Value.OperationId = $"{operation.Value.Summary.Replace(" ","_")}-{pathitem.Key}";
+                    }
+                }
+                openApiDocument.Paths.Remove("/$batch"); // Remove the Batch Operations as AutoREST can't handle the mimetype mixed/multipart type yet
+            }
+            if (bool.Parse(req.Headers["x-openapi-addmetadataoperations"].FirstOrDefault() ?? "false")){ // Add the / head and get operations
+                var pathitemroot =  new OpenApiPathItem();
+                var pathitemmeta =  new OpenApiPathItem();
+                var rootHeadOpHead = new OpenApiOperation(){OperationId = "root/head", Summary = "The root of the API, needed for any csrf processing"};
+                rootHeadOpHead.Responses.Add("200", new OpenApiResponse(){Description = ""});
+                pathitemroot.Operations.Add(OperationType.Head, rootHeadOpHead);
+                var rootHeadOpGet = new OpenApiOperation(){OperationId = "root/get", Summary = "The root of the API"};
+                rootHeadOpGet.Responses.Add("200", new OpenApiResponse(){Description = ""});
+                pathitemroot.Operations.Add(OperationType.Get, rootHeadOpGet);
+                openApiDocument.Paths.Add("/", pathitemroot);
+                var rootHeadOpMeta = new OpenApiOperation(){OperationId = "$metadata/get", Summary = "$Metadata endpoint"};
+                rootHeadOpMeta.Responses.Add("200", new OpenApiResponse(){Description = ""});
+                pathitemmeta.Operations.Add(OperationType.Get, rootHeadOpMeta);
+                openApiDocument.Paths.Add("/$metadata", pathitemmeta);
+            }
+            if (bool.Parse(req.Headers["x-openapi-adddiagnosticoperation"].FirstOrDefault() ?? "false")){ // add the /diagnostic operation for testing only
+                var pathItemDiag =  new OpenApiPathItem();
+                var diagHeadOpGet = new OpenApiOperation(){OperationId = "diagnostic/get", Summary = "The optional diagnostics policy for caching"};
+                diagHeadOpGet.Responses.Add("200", new OpenApiResponse(){Description = ""});
+                pathItemDiag.Operations.Add(OperationType.Get, diagHeadOpGet);
+                openApiDocument.Paths.Add("/diagnostic", pathItemDiag);
+            }
+            if (bool.Parse(req.Headers["x-openapi-truncate-description"].FirstOrDefault() ?? "false")){ // Info/Description parse and truncate to remove the EDM and keep us below 1000 chars.
+                openApiDocument.Info.Description = openApiDocument.Info.Description.Substring(0,openApiDocument.Info.Description.IndexOf("\n"));
+            }
+            if (req.Headers["x-openapi-version"].FirstOrDefault()?.StartsWith("3.0") ?? true){ // Export in the correct format OpenAPI 3.0 or swagger 2.0
+                return new OkObjectResult(openApiDocument.Serialize(OpenApiSpecVersion.OpenApi3_0, OpenApiFormat.Json));
+            }
+            else {return new OkObjectResult(openApiDocument.Serialize(OpenApiSpecVersion.OpenApi2_0, OpenApiFormat.Json));
+            }
+        }
+        public static string ApplyTransform(string input, XslCompiledTransform xslTrans, XsltArgumentList args = null) {
+            using (StringReader str = new StringReader(input) ){using (XmlReader xr = XmlReader.Create(str)) {using (StringWriter sw = new StringWriter()){
+                xslTrans.Transform(xr, args, sw);
+                return sw.ToString();
+            }}}
+        }
+    }
+}
