700710543679a490b87ab183454064a353200f2e
francois-dbag <36645726+francois-dbag@users.noreply.github.com>
2023-11-20 15:00:17 +0100

    Update index.js

--- FunctionApp/HttpTrigger1/index.js
+++ FunctionApp/HttpTrigger1/index.js
@@ -1,17 +1,70 @@
 module.exports = async function (context, req) {
     
-    context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
+    const csdl = require("odata-csdl");
+    const xslt4node = require('xslt4node');
+    const path = require('path');
+    const lib = require("../lib/csdl2openapi.js");
+    const xml = req.body;
+    const json = xml.startsWith("<") ? csdl.xml2json(xml, false) : JSON.parse(xml);
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+    var statusCode = 200;
+    var responseMessage = "OK";
+    var odataVersionTrace = "not set";
+    var openapi;
+    //check OData version to feed to correct target conversion approach as described by Oasis
+    if (json.$Version < "3.0") {
+      odataVersionTrace = "OData version < 3.0 converting to v4 using xslt4node";
+      var xsltpath = path.dirname(require.main.filename) + path.sep;
+      xslt4node.addLibrary(xsltpath + 'lib/xalan/xalan.jar');
+
+      xslt4node.transform(
+        {
+            xsltPath: xsltpath + 'OData-Version.xsl',
+            sourcePath: xml,
+            result: String
+        },
+        (err, result) => {
+            if (err) {
+              statusCode = 500;
+              responseMessage = 'Unable to deserialize XML: ' + err.message;
+            } else if (result == "") {
+              statusCode = 500;
+              responseMessage = 'Unable to parse input as OData v2: ' + err.message;
+            } else{
+              xslt4node.transform(
+                {
+                    xsltPath: xsltpath + 'V2-to-V4-CSDL.xsl',
+                    sourcePath: xml,
+                    result: target
+                },
+                (err, result) => {
+                    if (err) {
+                      statusCode = 500;
+                      responseMessage = 'Unable to convert OData v2 to v4: ' + err.message;
+                    } else {
+                      json = target;
+                    }
+                }
+              );
+            }
+      });
+    }else{
+      odataVersionTrace = "OData version >= 3.0 using csdl2openapi converter";
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
+    //do conversion only if no errors
+    if(statusCode == 200){
+      openapi = lib.csdl2openapi(json, {
+        scheme: req.headers['x-scheme'] || "https",
+        host: req.headers['x-host'] || "services.odata.org",
+        basePath: req.headers['x-basepath'] || "/service-root",
+        diagram: true,
+      });
+      responseMessage = JSON.stringify(openapi, null, 4);
     }
+    //return result
+    context.res = {
+        status: statusCode,
+        headers: {'content-type': 'application/json', 'odata-version-trace': odataVersionTrace},
+        body: responseMessage
+    };
 }
